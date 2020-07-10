273ece3e957ca20b19b0646f9be32aff13543614
Matt Rigg <riggm@amazon.com>
2020-07-10 10:26:28 -0700

    fix: inject sls stage into webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
+const webpack = require('webpack');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
-  devtool: slsw.lib.webpack.isLocal ? 'eval-source-map' : 'nosources-source-map',
+  devtool: slsw.lib.webpack.isLocal
+    ? 'eval-source-map'
+    : 'nosources-source-map',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   performance: {
-    hints: false
+    hints: false,
   },
   externals: [nodeExternals()],
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: slsw.lib.options.stage
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
         include: __dirname,
-        exclude: /node_modules/
-      }
-    ]
-  }
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
